df6e4f2d17c08b1255156fd8be101b0a6fe0dcf0
Ricky Effendi <rickricky.effendi@gmail.com>
2019-10-17 15:35:07 +0700

    sudah bisa ambil address titik tengah

--- BussMeStoryboard/ViewController.swift
+++ BussMeStoryboard/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 import GoogleMaps
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate {
 
     override func viewDidLoad() {
         let position = CLLocationCoordinate2D(latitude: 1.285, longitude: 103.848)
@@ -20,11 +22,27 @@ class ViewController: UIViewController {
         let camera = GMSCameraPosition.camera(withLatitude: 1.285, longitude: 103.848, zoom: 12)
         let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
         
+        mapView.delegate = self
+        
         marker.title = "Hello World"
         marker.map = mapView
         self.view = mapView
     }
 
-
+    func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+        let center = mapView.center
+        let coor = mapView.projection.coordinate(for: center)
+        //print(coor.latitude, coor.longitude)
+        
+        let address = CLGeocoder.init()
+        address.reverseGeocodeLocation(CLLocation.init(latitude: coor.latitude, longitude: coor.longitude)) { (places, error) in
+            if error == nil{
+                if let place = places{
+                    print(place[0].name!)
+                }
+            }
+        }
+    }
+    
 }
 
