c2d91c0233e30da7f8f46cb47f2439e1bec78b4b
Kevin Susanto <kevin.stnt@gmail.com>
2019-11-22 15:49:48 +0700

    Create model for collectionView

--- BussMeStoryboard/My Trips/MyTripsViewController.swift
+++ BussMeStoryboard/My Trips/MyTripsViewController.swift
@@ -8,23 +8,23 @@
 
 import UIKit
 
-class MyTripsViewController: UIViewController {
-
+class MyTripsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    @IBOutlet var recentTripsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ BussMeStoryboard/My Trips/TripsModel.swift
@@ -0,0 +1,17 @@
+//
+//  RouteModel.swift
+//  BussMeStoryboard
+//
+//  Created by Kevin Susanto on 22/11/19.
+//  Copyright © 2019 Ricky Effendi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct trips {
+    var route: String
+    var startPoint: String
+    var endPoint: String
+    var busCode: UIImage
+}
