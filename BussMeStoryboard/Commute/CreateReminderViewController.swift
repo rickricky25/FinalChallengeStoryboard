007afafba208c102afa68565ae5fab8729269b14
Kevin Susanto <kevin.stnt@gmail.com>
2019-12-04 16:16:44 +0700

    Fix Date Picker

--- BussMeStoryboard/Commute/CreateReminderViewController.swift
+++ BussMeStoryboard/Commute/CreateReminderViewController.swift
@@ -11,43 +11,44 @@ import UIKit
 class CreateReminderViewController: UIViewController {
 
     @IBOutlet var titleTextField: UITextField!
-    @IBOutlet var timeStart: UIButton!
-    @IBOutlet var timeBtnEnd: UIButton!
+    @IBOutlet var btnStartTime: UIButton!
+    @IBOutlet var btnEndTime: UIButton!
+    @IBOutlet var remindDatePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         formStyle()
         
-    
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        
+        view.addGestureRecognizer(tap)
+        
+//        DATE PICKER TIME
+//        remindDatePicker.isUserInteractionEnabled = false
+        remindDatePicker.isHidden = true
         
         
-    }
-    @IBAction func timeStart(_ sender: Any) {
-        let picker : UIDatePicker = UIDatePicker()
-        picker.datePickerMode = UIDatePicker.Mode.time
-        picker.addTarget(self, action: Selector(("dueDateChanged:")), for: UIControl.Event.valueChanged)
-//        let pickerSize : CGSize = picker.sizeThatFits(.zero)
-        picker.frame = CGRect(x: 0.0, y: 620, width: 414 , height: 200)
-        picker.backgroundColor = UIColor.systemBackground
-        self.view.addSubview(picker)
-    }
-    
-    @IBAction func timeBtnEnd(_ sender: Any) {
-        let picker : UIDatePicker = UIDatePicker()
-                picker.datePickerMode = UIDatePicker.Mode.time
-                picker.addTarget(self, action: Selector(("dueDateChanged:")), for: UIControl.Event.valueChanged)
-        //        let pickerSize : CGSize = picker.sizeThatFits(.zero)
-                picker.frame = CGRect(x: 0.0, y: 620, width: 414 , height: 200)
-                picker.backgroundColor = UIColor.systemBackground
-                self.view.addSubview(picker)
     }
     
     func formStyle() {
         titleTextField.borderStyle = UITextField.BorderStyle.roundedRect
-        timeStart.layer.cornerRadius = 10
-        timeBtnEnd.layer.cornerRadius = 10
     }
 
-   
-
+    @IBAction func btnStartTime(_ sender: Any) {
+//        remindDatePicker.isUserInteractionEnabled = true
+        remindDatePicker.isHidden = false
+        print("Start Time")
+        remindDatePicker.backgroundColor = UIColor.systemBackground
+    }
+    
+    @IBAction func btnEndTime(_ sender: Any) {
+        remindDatePicker.isUserInteractionEnabled = true
+        print("End Time")
+        remindDatePicker.backgroundColor = UIColor.systemBackground
+    }
+    
+//    DATE PICKER CHANGE
+    @IBAction func remindDatePickerVal(_ sender: UIDatePicker) {
+        
+    }
 }
