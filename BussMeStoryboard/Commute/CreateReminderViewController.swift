3654f4de56f17e2845a6c9d2ab66beda69c39ae9
Kevin Susanto <kevin.stnt@gmail.com>
2019-12-11 16:44:00 +0700

    Add Searchbar on Navigation Bar

--- BussMeStoryboard/Commute/CreateReminderViewController.swift
+++ BussMeStoryboard/Commute/CreateReminderViewController.swift
@@ -15,6 +15,7 @@ class CreateReminderViewController: UIViewController {
     @IBOutlet var btnEndTime: UIButton!
     @IBOutlet var remindDatePicker: UIDatePicker!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         formStyle()
@@ -26,7 +27,6 @@ class CreateReminderViewController: UIViewController {
 //        DATE PICKER TIME
 //        remindDatePicker.isUserInteractionEnabled = false
         remindDatePicker.isHidden = true
-        
         
     }
     
--- BussMeStoryboard/Commute/Table Controller/HalteListTableViewController.swift
+++ BussMeStoryboard/Commute/Table Controller/HalteListTableViewController.swift
@@ -23,8 +23,18 @@ class HalteListTableViewController: UITableViewController {
                    "Intermoda - Vanya Park",
                    "Vanya Park - Intermoda"]
     
-    var halte = [["a","b","c","d","e"],
-                 ["a","b","c","d","e"],
+    var halte = [["Green Cove","The Breeze",
+                  "CBD Utara 1","CBD Utara 3",
+                  "CBD Barat 1","CBD Barat 2",
+                  "Lobby AEON","ICE - 1",
+                  "Lobby ICE","ICE - 5"],
+                 
+                 ["ICE-5","Lobby ICE",
+                  "ICE - 1","Lobby AEON",
+                  "CBD Barat 2","CBD Barat 1",
+                  "CBD Utara 3","CBD Utara 1",
+                  "The Breeze","Green Cove"],
+                 
                  ["a","b","c","d","e"],
                  ["a","b","c","d","e"],
                  ["a","b","c","d","e"],
@@ -36,11 +46,27 @@ class HalteListTableViewController: UITableViewController {
                  ["a","b","c","d","e"],
                  ["a","b","c","d","e"]]
     
+    
+    
+//    for searching
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var isSearchBarEmpty: Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+//    -------
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
 
-       
+//        for searching
+        searchController.searchResultsUpdater = self as? UISearchResultsUpdating
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Cari Halte..."
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
     }
 //    Heading
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -55,7 +81,7 @@ class HalteListTableViewController: UITableViewController {
 //        return 1
         return self.section.count
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
 //        return halte.count
@@ -82,6 +108,94 @@ class HalteListTableViewController: UITableViewController {
         cell.textLabel?.font = UIFont.systemFont(ofSize: 17)
         return cell
     }
+    
+    func updateSearchResult(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        print(text)
+    }
+    
+//    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//        if isSearching { // this is just for SearchBar
+//            return autoCompleteDestino.count
+//        }
+//
+//        return autoCompleteDestino.count
+//
+//    }
+//
+//    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//        let selectedCell: UITableViewCell = tableView.cellForRow(at: indexPath)!
+//
+//        destinoSearch.text = selectedCell.textLabel!.text!
+//        busquedaDestinosText.text = selectedCell.textLabel!.text!
+//
+//    }}
+//}
+
+// for searching
+//extension CreateReminderViewController: UISearchResultsUpdating {
+//    func updateSearchResults(for searchController: UISearchController) {
+////        todo
+//    }
+//}
+
+//extension CreateReminderViewController: UITextFieldDelegate {
+//public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//
+//    let substring = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+//
+//    halte.searchDestinos(substring)
+//    return true
+//}}
+//
+//
+//extension CreateReminderViewController: UITableViewDataSource {
+//public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+//    let index = indexPath.row as Int
+//
+//    isSearching = true // this is just for SearchBar
+//    if isSearching {
+//        if let str = autoCompleteDestino[index].desDestino {
+//            cell.textLabel?.text = str
+//        }
+//    }
+//    return cell
+//}}
+//
+//extension CreateReminderViewController: UISearchBarDelegate {
+//func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//
+//    let stringOfUser = (busquedaDestinosText.text)
+//
+//    DestinoP.searchDestinos(stringOfUser!)
+//
+//    if  destinoSearch.text == "" {
+//        isSearching = false
+//        view.endEditing(true)
+//        tableView.reloadData()
+//        print(isSearching)
+//    } else {
+//        isSearching = true
+//        print(isSearching)
+//        print(autoCompleteDestino)
+//    }
+//
+//}}
 
 
+
+
+
+
+
+
+
+
+
+
+// ----------
 }
